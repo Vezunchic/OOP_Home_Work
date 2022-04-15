62e4722b284c3cb0df1cc1539206c0c2509b42d1
Vezunchic <piratAlexander@yandex.ru>
2022-04-15 12:38:12 +0300

    Добавлено задание к Lesson_7

---
+++ Lesson_7/Lesson_7_Test_1/ACoder.cs
@@ -0,0 +1,105 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Lesson_7_Test_1
+{
+    class ACoder : IСoder
+    {
+        public string Encode(string line, string path) //шифрование
+        {
+            char[] characterString = line.ToCharArray(); //разбиваем линию на символы
+            string code = LoadConfiguration(path); // получаем строку для сравнения
+            char[] codeChar = code.ToCharArray(); // разбиваем строку на символы
+            string newCoupLine = "";
+            for (int i = 0; i <= characterString.Length - 1; i++)
+            {
+                for (int j = 0; j <= codeChar.Length - 1; j++)
+                {
+                    if (characterString[i] == codeChar[j])
+                    {
+                        if (characterString[i] == codeChar[codeChar.Length - 1])
+                        {
+                            newCoupLine += codeChar[0].ToString();
+                        }
+                        else
+                        {
+                            newCoupLine += codeChar[j + 1].ToString();
+                        }
+
+
+                        break;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Шифрование методом А: {newCoupLine}");
+            return newCoupLine;
+        }
+        public string Decode(string line, string path)//дешифрования 
+        {
+            char[] characterString = line.ToCharArray(); //разбиваем линию на символы
+            string code = LoadConfiguration(path); // получаем строку для сравнения
+            char[] codeChar = code.ToCharArray(); // разбиваем строку на символы
+            string newCoupLine = "";
+            for (int i = 0; i <= characterString.Length - 1; i++)
+            {
+                for (int j = 0; j <= codeChar.Length - 1; j++)
+                {
+                    if (characterString[i] == codeChar[j])
+                    {
+                        if (characterString[i] == codeChar[0])
+                        {
+                            newCoupLine += codeChar[codeChar.Length - 1].ToString();
+                        }
+                        else
+                        {
+                            newCoupLine += codeChar[j - 1].ToString();
+                        }
+
+
+                        break;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Дешифрование меттодом А: {newCoupLine}");
+            return newCoupLine;
+        }
+
+
+        static string LoadConfiguration(string path) // подгрузка символов из файла для сравнения(шифрования) , если его нет то записывает файл
+        {
+            if (File.Exists(path))
+            {
+                var textToFile = File.ReadAllText(path);
+                try
+                {
+                    string config = JsonConvert.DeserializeObject<string>(textToFile);
+                    return config;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($" Ошибка: {ex.Message} ");
+                    return "";
+                }
+
+            }
+            else
+            {
+                string toFile = JsonConvert.SerializeObject("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"); // текст с которыи будет сравнивать для шифрования (если нет файла)
+                using (var sw = File.AppendText(path))
+                {
+                    sw.WriteLine(toFile);
+                }
+                return LoadConfiguration(path);
+            }
+
+
+
+        }
+    }
+}
---
+++ Lesson_7/Lesson_7_Test_1/BCoder.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Lesson_7_Test_1
+{
+    class BCoder : IСoder
+    {
+        public string Encode(string line, string path) //шифрование
+        {
+
+            char[] characterString = line.ToCharArray(); //разбиваем линию на символы
+            string code = LoadConfiguration(path); // получаем строку для сравнения
+            char[] codeChar = code.ToCharArray(); // разбиваем строку на символы
+            string newCoupLine = "";
+            for (int i = 0; i <= characterString.Length - 1; i++)
+            {
+                for (int j = 0; j <= codeChar.Length - 1; j++)
+                {
+                    if (characterString[i] == codeChar[j])
+                    {
+                        if (characterString[i] == codeChar[0])
+                        {
+                            newCoupLine += codeChar[codeChar.Length - 1].ToString();
+                            break;
+                        }
+                        
+                        if (characterString[i] == codeChar[codeChar.Length - 1])
+                        {
+                            newCoupLine += codeChar[0].ToString();
+                            break;
+                        }
+
+                        else 
+                        {
+                            var a = codeChar[codeChar.Length-1 - j] ;
+                            newCoupLine += a.ToString();
+
+                        }
+
+
+                        break;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Шифрование методом Б:{newCoupLine}");
+            return newCoupLine;
+        }
+        public string Decode(string line, string path) //дешифрования 
+        {
+            char[] characterString = line.ToCharArray(); //разбиваем линию на символы
+            string code = LoadConfiguration(path); // получаем строку для сравнения
+            char[] codeChar = code.ToCharArray(); // разбиваем строку на символы
+            string newCoupLine = "";
+            for (int i = 0; i <= characterString.Length - 1; i++)
+            {
+                for (int j = 0; j <= codeChar.Length - 1; j++)
+                {
+                    if (characterString[i] == codeChar[j])
+                    {
+                        if (characterString[i] == codeChar[codeChar.Length - 1])
+                        {
+                            newCoupLine += codeChar[0].ToString();
+                            break;
+                        }
+
+                        if (characterString[i] == codeChar[0])
+                        {
+                            newCoupLine += codeChar[codeChar.Length - 1] .ToString();
+                            break;
+                        }
+
+                        else
+                        {
+                            var a = codeChar[codeChar.Length - 1 - j];
+                            newCoupLine += a.ToString();
+
+                        }
+
+
+                        break;
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Дешифрование методом Б:{newCoupLine}");
+            return newCoupLine;
+        }
+        static string LoadConfiguration(string path) // подгрузка символов из файла для сравнения(шифрования) , если его нет то записывает файл
+        {
+            if (File.Exists(path))
+            {
+                var textToFile = File.ReadAllText(path);
+                try
+                {
+                    string config = JsonConvert.DeserializeObject<string>(textToFile);
+                    return config;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($" Ошибка: {ex.Message} ");
+                    return "";
+                }
+
+            }
+            else
+            {
+                string toFile = JsonConvert.SerializeObject("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"); // текст с которыи будет сравнивать для шифрования (если нет файла)
+                using (var sw = File.AppendText(path))
+                {
+                    sw.WriteLine(toFile);
+                }
+                return LoadConfiguration(path);
+            }
+
+
+
+        }
+    }
+}
